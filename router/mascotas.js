a0cc142d0343e8447084409c71ff86a919cca04e
Luis Vazquez <lv120189@gmail.com>
2020-10-04 01:19:33 -0300

    se agregaron vistas con ejs

--- router/mascotas.js
+++ router/mascotas.js
@@ -6,7 +6,7 @@ const Mascota = require('../models/mascota')
 router.get('/', async (req, res, next )=>{
  try{
     const arrayMascotasDB = await Mascota.find()
-    console.log(arrayMascotasDB);
+    /* console.log(arrayMascotasDB); */
 
     res.render('mascotas', {
         arrayMascotas:arrayMascotasDB
@@ -28,6 +28,7 @@ router.get('/crear', (req, res)=>{
 router.post('/', async (req, res)=>{
     const body = req.body
 try{
+    // await Mascota.create(body);
     const mascotaDB = new Mascota(body)
     await mascotaDB.save()
     res.redirect('/mascotas')
@@ -36,6 +37,52 @@ try{
     console.log(error)
 }
 })
+
+router.get('/:id', async(req, res)=>{
+   const id = req.params.id;
+
+   try{
+        const mascotaDB = await Mascota.findOne({_id:id})
+        console.log(mascotaDB );
+            res.render('detalle', {
+            mascota:mascotaDB,
+            error: false
+        })
+
+
+    }catch(error){
+        res.render('detalle', {
+            error: true,
+            mensaje:"no se encuentra el id seleccionado"
+        })
+    }
+})
+
+
+router.delete('/:id', async(req, res)=>{
+    const id = req.params.id;
+    try {
+       const mascotaDB =  await Mascota.findByIdAndDelete({_id:id})
+       if(mascotaDB){
+        res.json({
+            estado:true,
+            mensaje:"eliminado!"
+        })
+       } else{
+           res.json({
+               estado:false,
+               mensaje:"fallo eliminar!"
+           })
+       }   
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+})
+
+
+
 
 
 module.exports = router;
